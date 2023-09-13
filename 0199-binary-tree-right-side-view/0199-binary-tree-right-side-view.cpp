ae6fe3a6c5a5bddace41cc7e5f8c4603ed3f3809
Dev Pratap Singh <110956782+dev14325@users.noreply.github.com>
2023-09-14 01:21:59 +0530

    Time: 7 ms (16.83%), Space: 12.8 MB (5.81%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -1,23 +1,44 @@
 class Solution {
 public:
+vector<vector<int>> ans;
     vector<int> rightSideView(TreeNode* root) {
-        vector<int> result;
-        rightViewDFS(root, result, 0);
-        return result;
-    }
+          vector<int> res;
+        if(root==NULL) return res;
+        queue<TreeNode*> q;
+        q.push(root);
+      
+        while(!q.empty()){
+        int n = q.size();
+        vector<int> curr;
+        
+        for(int i=0;i<n;i++){
 
-    void rightViewDFS(TreeNode* node, vector<int>& result, int depth) {
-        if (!node) {
-            return;
+      
+        TreeNode* temp = q.front();
+
+        if(i==n-1) curr.push_back(temp->val);
+        
+        
+        q.pop();
+           
+           
+            if(temp->left) q.push(temp->left);
+            if(temp->right) q.push(temp->right);
+
+
+       }
+        ans.push_back(curr);
         }
+        
 
-        // If this is the first node encountered at the current depth, add it to the result.
-        if (depth == result.size()) {
-            result.push_back(node->val);
-        }
+      
 
-        // Recursively traverse the right subtree first (rightmost node at each level).
-        rightViewDFS(node->right, result, depth + 1);
-        rightViewDFS(node->left, result, depth + 1);
+      for(auto vec : ans){
+          if(!vec.empty()){
+              res.push_back(vec.back());
+          }
+      }
+
+      return res;
     }
 };
