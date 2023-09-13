9c516010076e5bfbba78f387101e5225a8bfee57
Dev Pratap Singh <110956782+dev14325@users.noreply.github.com>
2023-09-14 00:34:46 +0530

    Time: 2 ms (58.21%), Space: 12.4 MB (10.21%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -1,32 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        vector<vector<int>> ans;
-        if (root == nullptr) return ans;
 
+
+
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+
+       vector<vector<int>> ans;
+        if(root==NULL) return ans ;
         queue<TreeNode*> q;
         q.push(root);
-        bool reverseOrder = false;
-
-        while (!q.empty()) {
+        bool isRev = false;
+        while(!q.empty()){
             int n = q.size();
             vector<int> curr(n);
+            for(int i=0;i<n;i++){
 
-            for (int i = 0; i < n; i++) {
+       
                 TreeNode* node = q.front();
                 q.pop();
+                int idx = isRev ? n-1-i : i;
+                curr[idx] = node->val;
 
-                int index = reverseOrder ? n - 1 - i : i;
-                curr[index] = node->val;
+                // curr.push_back(node->val);
+               
 
-                if (node->left) q.push(node->left);
-                if (node->right) q.push(node->right);
-            }
-
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
+          } 
             ans.push_back(curr);
-            reverseOrder = !reverseOrder;
+            isRev = !isRev;
         }
 
-        return ans;
+      
+       
+     return ans;
+        
     }
 };
