b2c3342c76d15535d98aa727d3e1a52a9223dc39
Dev Pratap Singh <110956782+dev14325@users.noreply.github.com>
2023-09-24 01:54:48 +0530

    Time: 18 ms (72.17%), Space: 27.9 MB (35.59%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -1,50 +1,30 @@
 
 class Solution {
 public:
-  int maxPathSum(TreeNode* root) {
+  int maxPathSum(TreeNode* root, int& maxSum) {
+    if (!root)
+        return 0;
+
+    // Recursively calculate the maximum path sum for left and right subtrees
+    int leftMax = max(maxPathSum(root->left, maxSum), 0);
+    int rightMax = max(maxPathSum(root->right, maxSum), 0);
+
+    // Calculate the maximum path sum that includes the current node as the highest point
+    int currentMax = root->val + leftMax + rightMax;
+
+    // Update the overall maximum path sum
+    maxSum = max(maxSum, currentMax);
+
+    // Return the maximum path sum that can be extended upward to the parent node
+    return root->val + max(leftMax, rightMax);
+}
+
+int maxPathSum(TreeNode* root) {
     if (!root)
         return 0;
 
     int maxSum = INT_MIN;
-    int n = 0; // Number of nodes in the tree
-
-    // Helper function to calculate the maximum path sum starting from a given node
-    function<int(TreeNode*)> maxPathSumFromNode = [&](TreeNode* node) {
-        if (!node)
-            return 0;
-
-        int leftMax = maxPathSumFromNode(node->left);
-        int rightMax = maxPathSumFromNode(node->right);
-
-        int nodeMax = node->val;
-        if (leftMax > 0)
-            nodeMax += leftMax;
-        if (rightMax > 0)
-            nodeMax += rightMax;
-
-        maxSum = max(maxSum, nodeMax);
-
-        return max(node->val, max(leftMax, rightMax) + node->val);
-    };
-
-    // Calculate the maximum path sum considering all possible starting nodes
-    function<void(TreeNode*)> calculateMaxPathSum = [&](TreeNode* node) {
-        if (!node)
-            return;
-
-        maxPathSumFromNode(node); // Calculate max sum starting from this node
-
-        calculateMaxPathSum(node->left);
-        calculateMaxPathSum(node->right);
-
-        n++; // Increment the count of nodes visited
-    };
-
-    calculateMaxPathSum(root);
-
-    // Handle the case where all node values are non-negative
-    if (n == 1)
-        return maxSum;
+    maxPathSum(root, maxSum);
 
     return maxSum;
 }
