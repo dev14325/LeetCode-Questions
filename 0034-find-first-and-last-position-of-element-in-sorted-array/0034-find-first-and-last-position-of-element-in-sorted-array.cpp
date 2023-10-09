e8708b94859a4071a87219820e36bb341d442e06
Dev Pratap Singh <110956782+dev14325@users.noreply.github.com>
2023-10-09 23:23:45 +0530

    Time: 7 ms (51.15%), Space: 13.9 MB (37.79%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-         auto left = std::lower_bound(nums.begin(), nums.end(), target);
-    auto right = std::upper_bound(nums.begin(), nums.end(), target);
-
-    if (left == nums.end() || *left != target) {
-        return {-1, -1};
-    }
-
-    return {static_cast<int>(std::distance(nums.begin(), left)), static_cast<int>(std::distance(nums.begin(), right - 1))};
         
+
+       auto l = lower_bound(nums.begin(),nums.end(),target);
+       auto u = upper_bound(nums.begin(),nums.end(),target);
+
+       if(l==nums.end() ||  *l!=target) return {-1,-1};
+
+      int idx1 = distance(nums.begin(),l);
+      int idx2 = distance(nums.begin(),u-1);
+
+      return {idx1,idx2};
     }
 };
